452c54af3ae02bbc3369db1bce9d1f05b2b505d9
Ivan Kolpakov <ikolpakoff@gmail.com>
2019-11-16 11:56:03 +0300

    Created test controller

--- src/main/java/ru/airiva/simple/SimpleApplication.java
+++ src/main/java/ru/airiva/simple/SimpleApplication.java
@@ -3,12 +3,15 @@ package ru.airiva.simple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import ru.airiva.simple.controller.TestController;
 import ru.airiva.simple.domain.Person;
 import ru.airiva.simple.repository.PersonRepository;
 import ru.airiva.simple.service.impl.PersonServiceImpl;
 
-@EntityScan(basePackageClasses = {Person.class, PersonServiceImpl.class, PersonRepository.class})
-@SpringBootApplication
+@EntityScan(basePackageClasses = {Person.class})
+@SpringBootApplication(scanBasePackageClasses = {
+  PersonServiceImpl.class, PersonRepository.class, TestController.class
+})
 public class SimpleApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/ru/airiva/simple/controller/TestController.java
@@ -0,0 +1,17 @@
+package ru.airiva.simple.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+  @GetMapping
+  public ResponseEntity test() {
+    return ResponseEntity.ok().build();
+  }
+
+}
