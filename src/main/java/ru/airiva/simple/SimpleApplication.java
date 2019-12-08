11ce9d85dc7eb1592b9d9f14d5bc1f2344617959
Ivan Kolpakov <ikolpakoff@gmail.com>
2019-12-08 17:19:04 +0300

    Added swagger

--- src/main/java/ru/airiva/simple/SimpleApplication.java
+++ src/main/java/ru/airiva/simple/SimpleApplication.java
@@ -3,12 +3,15 @@ package ru.airiva.simple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import ru.airiva.simple.config.SwaggerConfig;
 import ru.airiva.simple.controller.TestController;
 import ru.airiva.simple.domain.Person;
 import ru.airiva.simple.repository.PersonRepository;
 import ru.airiva.simple.service.impl.PersonServiceImpl;
 
 @EntityScan(basePackageClasses = {Person.class})
+@Import({SwaggerConfig.class})
 @SpringBootApplication(scanBasePackageClasses = {
   PersonServiceImpl.class, PersonRepository.class, TestController.class
 })
